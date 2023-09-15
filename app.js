b841366f6375d7e15476a0fa2424ea3a1586e686
Haris <hh063063063@gmail.com>
2023-09-15 00:35:30 -0700

    Work Complete

---
+++ app.js
@@ -0,0 +1,122 @@
+// Q1
+
+var futureStudent = []
+
+// Q2
+
+var futureStudent = [     ]
+
+// Q3
+
+var arr = ["My ","name ","is ","Muhammad ","Haris"]
+
+console.log(arr);
+
+// Q4
+
+var arr1 = [2, 3, 4, 5]
+
+console.log(arr1);
+
+// Q5
+
+var arr2 = [true,false]
+
+console.log(arr2);
+
+// Q6
+
+var mixedArr = ["Karachi",1,true,"Quetta",false]
+
+console.log(mixedArr);
+
+// Q7
+
+var pakistanQualification = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil.","PhD"]
+
+document.write("<b>" + "Qualifications:"+ "</b>" + "<br>" + "<br>" + "1) " + pakistanQualification[0] + "<br>" + "2) " + pakistanQualification[1] + "<br>" + "3) " + pakistanQualification[2] + "<br>" + "4) " + pakistanQualification[3] + "<br>" + "5) " + pakistanQualification[4] + "<br>" + "6) " + pakistanQualification[5] + "<br>" + "7) " + pakistanQualification[6] + "<br>" + "8) " + pakistanQualification[7],"<br>")
+
+// Q8
+
+var arr3 = ["Furqan","Tanzeel","Istiyaq"]
+
+var arr4 = [320.,230.,480.]
+
+var arr5 = ["64%","46%","96%"]
+
+document.write("<b>" + "<br>" + "Score of " +  arr3[0] + " is " +  "Percantage: " + arr5[0] + "<br>" + "Score of " +  arr3[1] + " is " +  "Percantage: " + arr5[1] + "<br>" + "Score of " +  arr3[2] + " is " +  "Percantage: " + arr5[2] + "</b>","<br>");
+
+// Q9
+
+var arr6 = []
+
+arr6.unshift("Red", "Yellow", "Purple")
+
+arr6.push("Green")
+
+arr6.shift()
+
+arr6.pop()
+
+arr6.splice(1, 1, "Gray", "White")
+
+console.log(arr6);
+
+// Q10
+
+var arr7 = [320,230,480,120]
+
+var arr8 = [120,230,320,480]
+
+document.write("<br>" + "Scores of students : " + arr7 + "<br>" + "Ordered Scores of Students : " + arr8 + "<br>")
+
+// Q11
+
+var cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+
+document.write("<br>"+ "<b>" + "Cities list:" + "</b>" + "<br>" + cities)
+
+var cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+
+document.write("<br>" + "<b>" + "Selected Cities list:" + "</b>" + "<br>" + cities.slice(1, 3),"<br>")
+
+// Q12
+
+var arr9 = ["This","is","my","cat"]
+
+document.write("<br>","<b>","Array:","</b>","<br>",arr9)
+
+var arr9 = ["This ","is ","my ","cat"]
+
+document.write("<br>","<b>","String:","</b>","<br>",arr9[0],arr9[1],arr9[2],arr9[3])
+
+// Q13
+
+var fifo = ["Keyboard","Mouse","Printer","Monitor"]
+
+document.write("<br>","<b>","Devices:","</b>","<br>",fifo)
+
+var fifo = ["Keyboard","Mouse","Printer","Monitor"]
+
+document.write("<br>","<br>","Out:","<br>",fifo[0],"<br>","Out:","<br>",fifo[1],"<br>","Out:","<br>",fifo[2],"<br>","Out:","<br>",fifo[3],"<br>")
+
+// Q14
+
+var fifo = ["Keyboard","Mouse","Printer","Monitor"]
+
+document.write("<br>","<b>","Devices:","</b>","<br>",fifo)
+
+var fifo = ["Keyboard","Mouse","Printer","Monitor"]
+
+document.write("<br>","<br>","Out:","<br>",fifo[3],"<br>","Out:","<br>",fifo[2],"<br>","Out:","<br>",fifo[1],"<br>","Out:","<br>",fifo[0],)
+
+// Q15
+
+document.write("<br>","<br>","<select>",
+"<option>","Apple","</option>",
+"<option>","Samsung","</option>",
+"<option>","Motorola","</option>",
+"<option>","Nokia","</option>",
+"<option>","Sony","</option>",
+"<option>","Haier","</option>",
+"</select>")
